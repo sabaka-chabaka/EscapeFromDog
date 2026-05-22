315f52090a05936b6948b87468f861692eb8f35f
sabaka-chabaka <gamekimg360@gmail.com>
2026-05-22 13:45:55 +0400

    update

--- Source/EscapeFromDog/EscapeFromDog.Build.cs
+++ Source/EscapeFromDog/EscapeFromDog.Build.cs
@@ -8,7 +8,7 @@ public class EscapeFromDog : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
